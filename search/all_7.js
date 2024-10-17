6d34f51572454c16d7292d42beeaebfdd9848706
laursisask <laursisask@users.noreply.github.com>
2024-10-17 21:23:10 +0000

    deploy: 2b5a5009b7775e0aaf14bd4a3d0df6e2551ae697

---
+++ search/all_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hardware_0',['hardware',['../classaurora_1_1_hardware.html',1,'aurora::Hardware'],['../classaurora_1_1_hardware.html#a408e32e9bff8a58db98aabea9b4cfe51',1,'aurora::Hardware::Hardware()']]]
+];
---
+++ search/all_e.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['reading_20and_20running_20examples_0',['Reading and Running Examples',['../index.html#autotoc_md8',1,'']]],
+  ['readme_2emd_1',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
+  ['readytosavecal_2',['ReadyToSaveCal',['../classaurora_1_1_hardware.html#a294ba6486991b337181a83ec1a961eb7',1,'aurora::Hardware']]],
+  ['ringmod_3',['Ringmod',['../index.html#autotoc_md11',1,'']]],
+  ['running_20examples_4',['Reading and Running Examples',['../index.html#autotoc_md8',1,'']]]
+];
